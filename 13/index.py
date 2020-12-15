c3131a3af70d642f5fa5b81e73f8aa32aa6da2b8
AZZOUZ Mohamed <azzouzminfo@yahoo.fr>
2020-12-15 22:43:28 +0100

    Day 13 part 1 done

--- 13/index.py
+++ 13/index.py
@@ -1,2 +1,25 @@
+import math
+
 f = open('data.txt', 'r')
-instList = f.read().split('\n')
+busList = f.read().split('\n')
+
+def part1(busList):
+
+    earlyDepart = int(busList[0])
+    boutchou = []
+
+    for id in busList[1].split(','):
+        if id != 'x':
+            patachon = math.ceil(earlyDepart / int(id))
+            boutchou.append([int(id), patachon*int(id)])
+
+    earliest = None
+    for bout in boutchou:
+        if not earliest:
+            earliest = bout
+        elif bout[1] < earliest[1]:
+            earliest = bout
+    
+    return ((earliest[1] - earlyDepart) * earliest[0])
+
+print(part1(busList))
---
+++ 15/README.md
@@ -0,0 +1,9 @@
+# Day 15 challenge : Rambunctious Recitation
+
+Link to challenge : https://adventofcode.com/2020/day/15
+
+This challenge is using the Van Eck Sequence.
+
+## Best solution :
+
+None found yet
